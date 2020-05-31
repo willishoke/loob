0996fd299a962866fb53a00bfafff4a998f74eba
Willis Hoke <willis@minsky.local>
2020-05-31 01:17:59 -0700

    Setup initial class definitions

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+all:
+	g++-9 -std=c++17 main.cpp -o loob
+
+debug:
+	g++-9 -std=c++17 -g main.cpp -o loob
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <variant>
+#include "loob.h"
+
+int main(int argc, char** argv)
+{
+  Signal signal;
+  /*
+  signal = latch.process(Signal(Low()), Signal(High()));
+  std::cout << "Low, High: " << signal.high() << std::endl;
+  signal = latch.process(Signal(Low()), Signal(Low()));
+  std::cout << "Low, Low: " << signal.high() << std::endl;
+  signal = latch.process(Signal(High()), Signal(Low()));
+  std::cout << "High, Low: " << signal.high() << std::endl;
+  signal = latch.process(Signal(Low()), Signal(Low()));
+  signal = latch.process(Signal(High()), Signal(High()));
+  std::cout << "Low, Low: " << signal.high() << std::endl;
+  */
+  return 0;
+}
+
